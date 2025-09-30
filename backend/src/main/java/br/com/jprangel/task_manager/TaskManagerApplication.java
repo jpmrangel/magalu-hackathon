ee933752e2a45e130b97e864123d8cf29c606ec8
Diogo Bellini <152362491+diogo-bellini@users.noreply.github.com>
2025-09-30 19:00:36 -0300

    Revert "add: email logic"

--- backend/src/main/java/br/com/jprangel/task_manager/TaskManagerApplication.java
+++ backend/src/main/java/br/com/jprangel/task_manager/TaskManagerApplication.java
@@ -2,10 +2,8 @@ package br.com.jprangel.task_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
